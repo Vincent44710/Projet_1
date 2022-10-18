0019db5cc8da5ac3d2a2bd55f5d1f09fc437b581
Vincent <vinto0410@gmail.com>
2022-10-18 11:16:16 +0200

    maj nav burger
    Co-authored-by: LAMIRI Samy (SNCF RESEAU / INFRAPOLE PAYS DE LA LOIRE / Equipe parc SET 32) <samy.lamiri@reseau.sncf.fr>

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const burger = document.querySelector('.burger');
 
 burger.addEventListener('click',() =>{
     burger.classList.toggle('active')
-    // Quand ont va click sur la div .burger ca va l'activer et au reclick , la retirer, c'est le principe du toggle.
+    document.querySelector('nav').classList.toggle('show');
 });
